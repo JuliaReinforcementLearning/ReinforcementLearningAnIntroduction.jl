1be791487f5f00dbedebb82ff895c56b30d736db
Jun Tian <find_my_way@foxmail.com>
2020-08-19 18:40:37 +0800

    override RLBase.update!(model::AbstractEnvironmentModel, buffer::AbstractTrajectory) (#18)

--- src/ReinforcementLearningAnIntroduction.jl
+++ src/ReinforcementLearningAnIntroduction.jl
@@ -8,6 +8,13 @@ export RLIntro
 @reexport using ReinforcementLearningBase
 @reexport using ReinforcementLearningCore
 
+# !!! just a quick and dirty fix for
+# https://github.com/JuliaReinforcementLearning/ReinforcementLearningAnIntroduction.jl/issues/17
+function RLBase.update!(model::AbstractEnvironmentModel, buffer::AbstractTrajectory)
+    transitions = extract_experience(buffer, model)
+    isnothing(transitions) || update!(model, transitions)
+end
+
 include("environments/environments.jl")
 include("extensions/extensions.jl")
 
