40c29531fdecee0a01f8ca36f2c9e479997ed056
Jun Tian <tianjun.cpp@gmail.com>
2021-01-25 01:45:41 +0800

    sync local changes

---
+++ notebooks/Chapter04_Gambler_Problem.jl
@@ -0,0 +1,70 @@
+### A Pluto.jl notebook ###
+# v0.12.18
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f5cc0f04-5d99-11eb-3abe-bf3fccdac9e6
+begin
+	using ReinforcementLearning
+	using Flux
+	using Statistics
+	using Plots
+	using Distributions
+end
+
+# ╔═╡ 28cb401c-5d9a-11eb-2098-1939ea55d918
+begin
+	const pₕ = 0.4
+	const WinCapital = 100
+
+	decode_state(s::Int) = s - 1
+	encode_state(s::Int) = s + 1
+
+	function nextstep(s::Int, a::Int)
+		s = decode_state(s)
+		a = min(s, a)
+		if s == WinCapital || s==0
+			[(0., false,encode_state(s))=>1.0]
+		else
+			[
+				((s+a >= WinCapital ? 1.0 : 0.), false, encode_state(min(s+a, WinCapital))) => pₕ,
+			 	(0., false, encode_state(max(s-a, 0))) => 1-pₕ
+			]
+		end
+	end
+
+	struct GamblerProblemEnvModel <: AbstractEnvironmentModel
+		cache
+	end
+	
+	function GamblerProblemEnvModel()
+		GamblerProblemEnvModel(
+			Dict(
+				(s,a) => nextstep(s,a)
+				for s in 1:(WinCapital+1) for a in 1:WinCapital
+			)
+		)
+	end
+	
+	RLBase.state_space(m::GamblerProblemEnvModel) = Base.OneTo(WinCapital+1)
+	RLBase.action_space(m::GamblerProblemEnvModel) = Base.OneTo(WinCapital)
+	
+	(m::GamblerProblemEnvModel)(s, a) = m.cache[(s,a)]
+end
+
+# ╔═╡ 7cfd0a76-5d9c-11eb-35dc-176affb90ad3
+V = TabularVApproximator(;n_state=1+WinCapital,opt=Descent(1))
+
+# ╔═╡ bcede678-5d9c-11eb-05d0-fb25818336cf
+RLZoo.value_iteration!(V=V, model=GamblerProblemEnvModel(), γ=1.0, max_iter=1000)
+
+# ╔═╡ 005c156e-5d9e-11eb-11be-1b69be12b13b
+plot(V.table[2:end-1])
+
+# ╔═╡ Cell order:
+# ╠═f5cc0f04-5d99-11eb-3abe-bf3fccdac9e6
+# ╠═28cb401c-5d9a-11eb-2098-1939ea55d918
+# ╠═7cfd0a76-5d9c-11eb-35dc-176affb90ad3
+# ╠═bcede678-5d9c-11eb-05d0-fb25818336cf
+# ╠═005c156e-5d9e-11eb-11be-1b69be12b13b
---
+++ notebooks/Chapter04_Grid_World.jl
@@ -0,0 +1,81 @@
+### A Pluto.jl notebook ###
+# v0.12.18
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 604afcfc-5d9d-11eb-0e2d-4971e8c87824
+begin
+	using ReinforcementLearning
+	using Flux
+	using Statistics
+	using Plots
+	using Distributions
+end
+
+# ╔═╡ be402764-5e02-11eb-0978-bb9f65076f60
+md"""
+Again, we use a environment model to describe the **Grid World** in **Chapter 4.2**.
+"""
+
+# ╔═╡ f2de50d6-5e02-11eb-0c19-2756e3a8fe68
+begin
+	isterminal(s::CartesianIndex{2}) = s == CartesianIndex(1,1) || s == CartesianIndex(4,4) 
+
+	function nextstep(s::CartesianIndex{2}, a::CartesianIndex{2})
+		s′ = s + a
+		if isterminal(s) || s′[1] < 1 || s′[1] > 4 || s′[2] < 1 || s′[2] > 4
+			s′ = s
+		end
+		r = isterminal(s) ? 0. : -1.0
+		[(r, isterminal(s′), LinearIndices((4,4))[s′]) => 1.0]
+	end
+
+	const ACTIONS = [
+		CartesianIndex(-1, 0),
+		CartesianIndex(1,0),
+		CartesianIndex(0, 1),
+		CartesianIndex(0, -1)
+	]
+
+	struct GridWorldEnvModel <: AbstractEnvironmentModel
+		cache
+	end
+	
+	GridWorldEnvModel() = GridWorldEnvModel(
+		Dict(
+			(s, a) => nextstep(CartesianIndices((4,4))[s], ACTIONS[a])
+			for s in 1:16 for a in 1:4
+		)
+	)
+	
+	(m::GridWorldEnvModel)(s, a) = m.cache[(s,a)]
+	
+	RLBase.state_space(m::GridWorldEnvModel) = Base.OneTo(16)
+	RLBase.action_space(m::GridWorldEnvModel) = Base.OneTo(4)
+end
+
+# ╔═╡ f02b3240-5e03-11eb-3009-8fa372d61d3d
+V = TabularVApproximator(n_state=16, opt=Descent(1.0))
+
+# ╔═╡ fe9a730e-5e03-11eb-26e2-8b94166c0332
+p = TabularRandomPolicy(table=Dict(s => fill(0.25, 4) for s in 1:16))
+
+# ╔═╡ f27da2fa-5e04-11eb-268f-45865581a8c9
+model = GridWorldEnvModel()
+
+# ╔═╡ 4b8ecf0c-5e04-11eb-38a1-839f6cb37e78
+policy_evaluation!(V=V, π=p, model=model, γ=1.0)
+
+# ╔═╡ 6484f504-5e04-11eb-1286-9ba6803d9f2b
+reshape(V.table, 4,4)
+
+# ╔═╡ Cell order:
+# ╠═604afcfc-5d9d-11eb-0e2d-4971e8c87824
+# ╟─be402764-5e02-11eb-0978-bb9f65076f60
+# ╠═f2de50d6-5e02-11eb-0c19-2756e3a8fe68
+# ╠═f02b3240-5e03-11eb-3009-8fa372d61d3d
+# ╠═fe9a730e-5e03-11eb-26e2-8b94166c0332
+# ╠═f27da2fa-5e04-11eb-268f-45865581a8c9
+# ╠═4b8ecf0c-5e04-11eb-38a1-839f6cb37e78
+# ╠═6484f504-5e04-11eb-1286-9ba6803d9f2b
